0a92c838eff691c96f22f91d576e63ef6d3291bd
sergiopalooza <sergioflores836@gmail.com>
2020-05-25 12:47:54 -0500

    finished conversion, added specific tasks to reopen

--- todoistReopen.ts
+++ todoistReopen.ts
@@ -1,9 +1,31 @@
-const res = await fetch("https://api.todoist.com/rest/v1/tasks/2860758634/reopen", {
-    method: "POST",
-    headers: {
-     "Authorization": `Bearer ${Deno.env.get('TODO_AUTH')}`
+const taskIds: Array<string> = [
+    "2860758634",
+    "2860758746",
+    "2860758827",
+    "2860758890",
+    "2860759323",
+    "2860759392",
+    "2860759514",
+    "2860759978",
+    "2872976681",
+    "2881372327",
+    "3123760866",
+    "3222988201",
+    "3407676009",
+    "3412514316"
+];
+
+
+async function openTasks (tasks: Array<string>){
+    for (const task of tasks) {
+        console.log(task);
+        await fetch(`https://api.todoist.com/rest/v1/tasks/${task}/reopen`, {
+            method: "POST",
+            headers: {
+                "Authorization": `Bearer ${Deno.env.get('TODO_AUTH')}`
+            }
+        })
     }
-});
+}
 
-console.log(res);
-
+await openTasks(taskIds);
